3e83e026b022a67d625e17efb33d6bbe1ed2b38a
Ilya Boyandin <ilyabo@gmail.com>
2017-11-10 23:52:13 +0100

    Removed gatsby-source-filesystem for fragments from config

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,13 +20,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/fragments`,
-        name: 'fragments'
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         path: `${__dirname}/src/pages`,
         name: 'pages'
       }
