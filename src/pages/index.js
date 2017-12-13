59fc201f16e6a884a789de2fa351a63c7c4eeab9
Ilya Boyandin <ilyabo@gmail.com>
2017-12-13 19:25:37 +0100

    Homepage column reordering

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import Link from 'gatsby-link'
 import _ from 'lodash'
+import { css } from 'glamor'
+
+const styles = {
+  columns: css({
+    display: 'flex',
+    '@media screen and (max-width: 768px)': {
+      flexDirection: 'column',
+    }
+  }),
+  aktuelles: css({
+    '@media screen and (max-width: 768px)': {
+      order: -1
+    }
+  })
+}
 
 
 const NewsColumn = ({ newsRemarks, numItemsToShow }) => {
@@ -10,7 +25,10 @@ const NewsColumn = ({ newsRemarks, numItemsToShow }) => {
   const { edges: newsEntries } = newsRemarks
   const news = _.take(newsEntries, numItemsToShow)
   return (
-    <div className="column">
+    <div
+      className="column"
+      {...styles.aktuelles}
+    >
       <h2 className="title">Aktuelles</h2>
       {news.map(({ node: newsItem }) => {
         return (
@@ -64,7 +82,10 @@ export default (
     }
   }) =>
   <section className="section">
-    <div className="columns">
+    <div
+      className="columns"
+      {...styles.columns}
+    >
       <div className="column">
         <Goals {...goals} />
       </div>
