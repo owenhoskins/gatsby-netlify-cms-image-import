257eb2b30cb903a0ff26b00cbc4eda0555a18201
Ilya Boyandin <ilyabo@gmail.com>
2017-12-13 22:12:43 +0100

    Using absolute path to netlify-login.js

--- src/html.js
+++ src/html.js
@@ -43,7 +43,7 @@ module.exports = class HTML extends React.Component {
             dangerouslySetInnerHTML={{ __html: this.props.body }}
           />
           {this.props.postBodyComponents}
-          <script src="js/netlify-login.js"></script>
+          <script src="/js/netlify-login.js"></script>
         </body>
       </html>
     )
