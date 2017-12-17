453414a9f4780020b219827aa7ff46afe6ad1f93
Ilya Boyandin <ilyabo@gmail.com>
2017-12-17 10:41:15 +0100

    Showing date in news items

--- src/templates/news-item.js
+++ src/templates/news-item.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Helmet from 'react-helmet';
+import { formatDate } from '../utils'
 
 export default function Template({ data }) {
   const { markdownRemark: post } = data;
@@ -7,7 +8,12 @@ export default function Template({ data }) {
     <section className="section">
       <Helmet title={`${post.frontmatter.title}`} />
       <div className="content">
-        <h1 className="title is-size-2 has-text-info is-bold-light">{post.frontmatter.title}</h1>
+        <h1 className="title is-size-2 has-text-info is-bold-light">
+          {post.frontmatter.title}
+        </h1>
+        <p style={{ marginTop: '-.5rem', marginBottom: '2rem' }}>
+          {formatDate(post.frontmatter.date)}
+        </p>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>
     </section>
