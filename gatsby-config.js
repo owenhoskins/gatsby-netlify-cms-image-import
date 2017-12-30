51eaad5daa56b8765765c22925e678f69990b3db
Ilya Boyandin <ilyabo@gmail.com>
2017-12-30 01:08:34 +0100

    Carousel images moved to static/img/carousel

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        path: `${__dirname}/src/img/carousel`,
+        path: `${__dirname}/static/img/carousel`,
         name: 'carousel-images'
       }
     },
